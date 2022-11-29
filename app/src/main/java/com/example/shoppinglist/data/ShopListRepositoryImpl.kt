ecd6a54ee3c68a631fd9f4d990d994482ccdf13a
khush-byte <khush4450@gmail.com>
2022-11-29 13:54:29 +0500

    add a local database

--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -1,78 +1,72 @@
 package com.example.shoppinglist.data
 
+import android.app.Application
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import com.example.shoppinglist.domain.ShopItem
 import com.example.shoppinglist.domain.ShopListRepository
 
 //Implementation of the interface
-object ShopListRepositoryImpl : ShopListRepository {
-    //Creation of the live date variable to store a list of items
-    private var shopListLiveData = MutableLiveData<List<ShopItem>>()
+class ShopListRepositoryImpl(
+    application: Application
+) : ShopListRepository {
+    private val shopListDao = AppDatabase.getInstance(application).shopListDao()
+    private val mapper = ShopListMapper()
 
-    //Item sorting
-    private var shopList = sortedSetOf<ShopItem>({ o1, o2 -> o1.id.compareTo(o2.id) })
-
-    //Variable for counting id number
-    var autoIncrementId = 0
 
     //Implementation of the add function
     override fun addShopItem(shopItem: ShopItem) {
-        //If item has default value than set auto increment
-        if (shopItem.id == ShopItem.UNDEFINED_ID) {
-            shopItem.id = autoIncrementId++
-        }
-        //Add a new item to the list
-        shopList.add(shopItem)
-        //Add the list to the live data
-        updateList()
-    }
-
-    //Implementation of the edit function
-    override fun editShopItem(shopItem: ShopItem) {
-        //Deleting the old version of element
-        val oldElement = getShopItem(shopItem.id)
-        deleteShopItem(oldElement)
-        //Adding the new version of element
-        addShopItem(shopItem)
+        shopListDao.addShopItem(mapper.mapEntityToDbModel(shopItem))
     }
 
     //Implementation of the delete function
     override fun deleteShopItem(shopItem: ShopItem) {
-        shopList.remove(shopItem)
-        updateList()
+        shopListDao.deleteShopItem(shopItem.id)
+    }
+
+    //Implementation of the edit function
+    override fun editShopItem(shopItem: ShopItem) {
+        shopListDao.addShopItem(mapper.mapEntityToDbModel(shopItem))
     }
 
     //Implementation of the get item function
     override fun getShopItem(shopItemId: Int): ShopItem {
-        return shopList.find {
-            it.id == shopItemId
-            //Throw exception if id does not exist
-        } ?: throw java.lang.RuntimeException("Element with id $shopItemId not found")
+        val dbModel = shopListDao.getShopItem(shopItemId)
+        return mapper.mapDbModelToEntity(dbModel)
     }
 
     //Implementation of the get list function
-    override fun getShopList(): LiveData<List<ShopItem>> {
-        shopListLiveData.value = shopList.toList()
-        return shopListLiveData
+//    override fun getShopList(): LiveData<List<ShopItem>> = MediatorLiveData<List<ShopItem>>().apply {
+//        addSource(shopListDao.getShopList()){
+////            if(it.size>10){
+//    //                value = it
+////            }
+//            value = mapper.mapListDbModelToListEntity(it)
+//        }
+//    }
+
+    override fun getShopList(): LiveData<List<ShopItem>> = Transformations.map(
+        shopListDao.getShopList()
+    ) {
+        mapper.mapListDbModelToListEntity(it)
     }
 
-    //Implementation of the list update function
-    override fun setShopList(shopList: List<ShopItem>) {
-        cleanShopList()
-        for (item in shopList) {
-            val shopItem = ShopItem(item.name, item.count, item.enabled)
-            addShopItem(shopItem)
-        }
-    }
-
-    //Passing the item's list to the live date
-    private fun updateList() {
-        shopListLiveData.value = shopList.toList()
-    }
-
-    override fun cleanShopList() {
-        shopList.clear()
-        updateList()
-    }
+//    //Implementation of the list update function
+//    override fun setShopList(shopList: List<ShopItem>) {
+//        cleanShopList()
+//        for (item in shopList) {
+//            val shopItem = ShopItem(item.name, item.count, item.enabled)
+//            addShopItem(shopItem)
+//        }
+//    }
+//
+//    //Passing the item's list to the live date
+//    private fun updateList() {
+//        shopListLiveData.value = shopList.toList()
+//    }
+//
+//    override fun cleanShopList() {
+//        shopList.clear()
+//        updateList()
+//    }
 }
--- app/src/main/java/com/example/shoppinglist/domain/ShopListRepository.kt
+++ app/src/main/java/com/example/shoppinglist/domain/ShopListRepository.kt
@@ -9,6 +9,6 @@ interface ShopListRepository {
     fun deleteShopItem(shopItem: ShopItem)
     fun getShopItem(shopItemId: Int): ShopItem
     fun getShopList(): LiveData<List<ShopItem>>
-    fun setShopList(shopList: List<ShopItem>)
-    fun cleanShopList()
+    //fun setShopList(shopList: List<ShopItem>)
+    //fun cleanShopList()
 }
